150833d91141b1a399dedb98c9331dd54423e003
jusbenny0 <stardestrojr@gmail.com>
2025-01-13 16:46:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='text-red-500 font-bold'>
+      HI, this is a test for Tailwind
+    </div>
+  );
+}
+
+export default App;
